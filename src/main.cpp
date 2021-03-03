9919e69fa0a8fcca3f1f1b2b72b350eb960da032
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-03-03 12:31:09 +0100

    Add option to print version

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <cstdio>
 
+#include "hello-world-conf.h"
+
+
 int main(int argc, char *argv[])
 {
 	if (argc > 2) {
-		std::fprintf(stderr, "Usage: %s [name]\n", argv[0]);
+		std::fprintf(stderr, "Usage: %s [-v|name]\n", argv[0]);
 		return 1;
 	}
 
-	std::printf("Hello, %s!\n", argc==2 ? argv[1] : "world");
+	if (argc == 1) {
+		std::printf("Hello, world!\n");
+	} else if (argc == 2) {
+		if (argv[1][0] == '-' && argv[1][1] == 'v')
+			std::printf("Version %d.%d\n", VERSION_MAJOR, VERSION_MINOR);
+		else
+			std::printf("Hello, %s\n", argv[1]);
+	}
 	return 0;
 }
