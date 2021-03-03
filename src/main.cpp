42270703b6c695c92a61b1e9ca76fa14b9014c18
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-03-03 12:09:35 +0100

    Add default greeting

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		std::fprintf(stderr, "Usage: %s name\n", argv[0]);
+	if (argc > 2) {
+		std::fprintf(stderr, "Usage: %s [name]\n", argv[0]);
 		return 1;
 	}
-	std::printf("Hello, %s!\n", argv[1]);
+
+	std::printf("Hello, %s!\n", argc==2 ? argv[1] : "world");
 	return 0;
 }
